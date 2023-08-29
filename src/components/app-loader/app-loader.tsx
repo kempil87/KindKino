f00897fc76e9abcab83a5819635a2d9a58e50476
AlexRixten <rixten.09@gmail.com>
2023-08-29 19:38:14 +0300

    add pages

--- src/components/app-loader/app-loader.tsx
+++ src/components/app-loader/app-loader.tsx
@@ -1,33 +1,33 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 
 import { Logo } from '~/components/logo/logo';
 
-let timer: NodeJS.Timeout;
+// let timer: NodeJS.Timeout;
 
 export const AppLoader = () => {
   const loadingRef = useRef<HTMLDivElement>(null);
 
-  const handler = () => {
-    timer = setTimeout(() => loadingRef.current?.classList.add('!hidden'), 500);
-  };
+  // const handler = () => {
+  //   timer = setTimeout(() => loadingRef.current?.classList.add('!hidden'), 500);
+  // };
 
-  useEffect(() => {
-    if (document.readyState === 'complete') {
-      handler();
-    } else {
-      window.addEventListener('load', handler);
-
-      return () => {
-        window.removeEventListener('load', handler);
-        clearTimeout(timer);
-      };
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (document.readyState === 'complete') {
+  //     handler();
+  //   } else {
+  //     window.addEventListener('load', handler);
+  //
+  //     return () => {
+  //       window.removeEventListener('load', handler);
+  //       clearTimeout(timer);
+  //     };
+  //   }
+  // }, []);
 
   return (
     <div
       ref={loadingRef}
-      className="fixed inset-0 z-[1000] flex flex-col items-center justify-center gap-3 bg-black text-center"
+      // className="fixed inset-0 z-[1000] flex flex-col items-center justify-center gap-3 bg-black text-center"
     >
       <Logo />
       <p className="text-xl">Загрузка...</p>
